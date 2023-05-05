19b4f82850f1b049325d25d4eca427debfcec332
jenikinsdocker <jenikinsdocker@gmail.com>
2023-05-05 07:25:29 +0530

    modify main class

--- src/main/java/com/java/bootrest/SrpingbootRestRealTimeApplication.java
+++ src/main/java/com/java/bootrest/SrpingbootRestRealTimeApplication.java
@@ -2,9 +2,17 @@ package com.java.bootrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SrpingbootRestRealTimeApplication {
+public class SrpingbootRestRealTimeApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SrpingbootRestRealTimeApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SrpingbootRestRealTimeApplication.class, args);
